f0232064434ee07e2f7685074f31fe93a6cd7015
guifengz <andyjoe025@qq.com>
2017-08-21 23:02:34 +0800

    update router

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './pages/home/home.jsx'
-// import About from './pages/about/about.jsx'
+import About from './pages/about/about.jsx'
 // import Inbox from './pages/inbox/inbox.jsx'
 // console.log(1,hashHistory)
 
@@ -11,6 +11,7 @@ ReactDOM.render((
   <BrowserRouter>
     <div>
       <Route path="/" component={Home}/>
+      <Route path="/about" component={About}/>
     </div>
   </BrowserRouter>
 ), document.getElementById('app'));

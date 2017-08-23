d855290eae517931c2eee0e6da09b5958b71475a
guifengz <andyjoe025@qq.com>
2017-08-23 11:02:16 +0800

    process component inline style

--- app/pages/about/about.jsx
+++ app/pages/about/about.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+// using inline style in component
+const titleStyle = {
+  color: 'blue',
+  fontSize: '30px'
+};
+
 class About extends Component{
   render() {
     return (
       <div>
-        <h2>this is about page</h2>
+        <h2 style={titleStyle}>this is about page</h2>
       </div>
     )
   }
